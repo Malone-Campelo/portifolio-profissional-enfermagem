049ff89271ba925d0b9d1a905da3f7d8cf9feef7
Malone-Campelo <malonycampelo1@gmail.com>
2024-06-16 23:08:29 -0300

    projeto finalizado

---
+++ src/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+            navList.classList.remove('active');
+        });
+    });
+});
