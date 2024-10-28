c8973990353b16376fdca2bc9f8eb1a3fe897f01
Malone-Campelo <malonycampelo1@gmail.com>
2024-10-27 22:39:48 -0300

    implementando back

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+async function sendEmail(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    try {
+        const response = await fetch('http://localhost:5000/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, message })
+        });
+        
+        const result = await response.json();
+        alert(result.message);
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao enviar a mensagem. Tente novamente mais tarde.');
+    }
+}
